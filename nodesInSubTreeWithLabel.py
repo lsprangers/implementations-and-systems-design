455d1a9db4bc7701f8d858ee6c14201f813610aa
Luke Sprangers <sprangersluke@gmail.com>
2023-01-12 10:33:41 -0500

    1519

---
+++ nodesInSubTreeWithLabel.py
@@ -0,0 +1,28 @@
+# problem 1519
+# was so close myself
+from typing import List 
+from collections import defaultdict, Counter
+
+class Solution:
+    def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
+        tree = defaultdict(list)
+        for s,e in edges:
+            tree[s].append(e)
+            tree[e].append(s)
+        
+        res = [0] * n
+        
+        def dfs(node, par):
+            nonlocal res
+            count = Counter()
+            for nei in tree[node]:
+                if nei != par:
+                    count += dfs(nei, node)
+            
+            count[labels[node]] += 1
+            res[node] = count[labels[node]]
+            
+            return count
+        
+        dfs(0,-1)
+        return res
