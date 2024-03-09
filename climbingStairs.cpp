4f2c6c242374d830e7274c2d9a1359239f0d0647
Luke Sprangers <sprangersluke@gmail.com>
2024-03-09 08:53:43 -0500

    Leet 4 today

--- climbingStairs.cpp
+++ climbingStairs.cpp
@@ -1,26 +1,21 @@
-// for any number n = [n-1] + [n-2]
-//  because we can take either one or two steps at the beginning
-//  & if we take 1 then it's the same solution as n-1, and if we 
-//      take 2 it's the same solution as n-2
+// 0, 1, 2, 3, 5 -- fibonacci seq
 #include <vector>;
-
+using namespace std;
+// 70
 class Solution {
 public:
     int climbStairs(int n) {
-        std::vector< unsigned int > fibs;
-        fibs.push_back(1);
-        fibs.push_back(2);
-
-        if (n < 3) {
-            return fibs[n-1];
+        vector<int> tabb;
+        tabb.push_back(0);
+        tabb.push_back(1);
+        tabb.push_back(2);
+        for(int i = 3; i <=n; i++) {
+            tabb.push_back(
+                tabb[i - 1] // 1 step
+                +
+                tabb[i - 2] // 2 steps
+            );
         }
-        else {
-            int i = 2;
-            while(i < n+2) {
-                fibs.push_back(fibs[i-2] + fibs[i-1]);
-                i++;
-            }
-            return fibs[n-1];
-        }
+        return(tabb[n]);
     }
 };
